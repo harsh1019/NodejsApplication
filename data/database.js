ec710804dacdad46b625557228df1011619c8f5e
harshrulaniya <harshrulaniya2001@gmail.com>
2023-06-02 21:54:57 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -5,8 +5,8 @@ export const connectDb = () =>{
 
 mongoose.connect(process.env.MONGO_URI,{
     dbName:"firstapp",
-}).then(()=>{
-    console.log("Database Connected");
+}).then((c)=>{
+    console.log(`Database Connected with ${c.connection.host}`);
 }).catch((e)=>{
    console.log(e);
 });
